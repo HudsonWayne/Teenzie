0c6c574b6bdea6c5fe9c61c855e49b7b9b211c27
HudsonWayne <waynewaynebenhura@gmail.com>
2024-06-05 14:07:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import "../../tenzies/src/App.css"
+// import Die from "../../tenzies/src/Die"
+
+// const [dice, setDice] = React.useState(allNewDice())
+
+
+// export default function App(){
+//   function allNewDice(){
+//     const newDice = []
+//     for (let i = 0; i < 10; i++) {
+//       newDice.push(Math.floor(Math.random() * 3));
+//     }
+//     return newDice
+//   }
+//   console.log(allNewDice())
+
+//   const diceElements = dice.map(die =><Die value= {die} />)
+
+  
+
+//   return(
+//     <main>
+//       <div className="dice-container">
+//         {diceElements}
+        
+//       </div>
+//     </main>
+//   )
+// }
+
+
+// <h1>Tenzies</h1>
+//       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
+import React, { useEffect, useState } from "react"; // Import useState hook
+import "../../tenzies/src/App.css"; // Import your CSS file (assuming path is correct)
+import Die from "../../tenzies/src/Die"; // Import Die component
 
 function App() {
+  const [dice, setDice] = useState([]); // Initialize state with random dice values
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * 6));
+    }
+     console.log("new dice ", newDice)
+    return newDice;
+  }
+
+  useEffect(()=>{
+
+    setDice(allNewDice())
+  },[])
+  function rollDice(){
+    setDice(old =>{
+      return allNewDice()
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="dice-container">
+        { dice.map((die , index) => <Die value={die} key={index.value} />)}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
+    </main>
   );
 }
 
